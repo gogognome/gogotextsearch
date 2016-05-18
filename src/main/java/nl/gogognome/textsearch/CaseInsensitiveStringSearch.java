7cf813c46a93d960a92167fb6e639e0b8a479c03
Sander Kooijmans <sander@gogognome.nl>
2016-05-18 23:20:10 +0200

    Added case insensitve search.

---
+++ src/main/java/nl/gogognome/textsearch/CaseInsensitiveStringSearch.java
@@ -0,0 +1,32 @@
+package nl.gogognome.textsearch;
+
+// Code from http://stackoverflow.com/questions/1126227/indexof-case-sensitive
+public class CaseInsensitiveStringSearch {
+
+    public int indexOfIgnoreCase(final String chkstr, final String searchStr) {
+        int i = 0;
+        if (chkstr != null && searchStr != null) {
+            if (searchStr.isEmpty()) {
+                return 0;
+            }
+            int serchStrLength = searchStr.length();
+            char[] searchCharLc = new char[serchStrLength];
+            char[] searchCharUc = new char[serchStrLength];
+            searchStr.toUpperCase().getChars(0, serchStrLength, searchCharUc, 0);
+            searchStr.toLowerCase().getChars(0, serchStrLength, searchCharLc, 0);
+            int j = 0;
+            for (int checkStrLength = chkstr.length(); i < checkStrLength; i++) {
+                char charAt = chkstr.charAt(i);
+                if (charAt == searchCharLc[j] || charAt == searchCharUc[j]) {
+                    if (++j == serchStrLength) {
+                        return i - j + 1;
+                    }
+                } else { // faster than: else if (j != 0) {
+                    i = i - j;
+                    j = 0;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/nl/gogognome/textsearch/CaseInsensitiveStringSearchTest.java
@@ -0,0 +1,30 @@
+package nl.gogognome.textsearch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CaseInsensitiveStringSearchTest {
+    @Test
+    public void testIndexOfIgnoreCase() {
+        CaseInsensitiveStringSearch caseInsensitiveStringSearch = new CaseInsensitiveStringSearch();
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("A", "A"), 0);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("a", "A"), 0);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("A", "a"), 0);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("a", "a"), 0);
+
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("a", "ba"), -1);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("ba", "a"), 1);
+
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("Royal Blue", " Royal Blue"), -1);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase(" Royal Blue", "Royal Blue"), 1);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("Royal Blue", "royal"), 0);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("Royal Blue", "oyal"), 1);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("Royal Blue", "al"), 3);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("", "royal"), -1);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("Royal Blue", ""), 0);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("Royal Blue", "BLUE"), 6);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("Royal Blue", "BIGLONGSTRING"), -1);
+        assertEquals(caseInsensitiveStringSearch.indexOfIgnoreCase("Royal Blue", "Royal Blue LONGSTRING"), -1);
+    }
+}
