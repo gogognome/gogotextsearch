c943b75053de83114644f00ac830b3b439c6e0e7
Sander Kooijmans <sander@gogognome.nl>
2017-09-14 22:42:59 +0200

    Skip byte order mark (BOM) when reading UTF-8 or UTF-16 file.

--- src/main/java/nl/gogognome/textsearch/textfile/CachedSearchableTextFile.java
+++ src/main/java/nl/gogognome/textsearch/textfile/CachedSearchableTextFile.java
@@ -33,7 +33,20 @@ public class CachedSearchableTextFile extends BaseSearchableTextFile {
         }
 
         byte[] bytes = Files.readAllBytes(file.toPath());
-        textFileSearch = new SuffixArrayTextFileSearch(new String(bytes, charset));
+        int offset = 0;
+        int length = bytes.length;
+        if (length > 3 && bytes[0] == 0xef && bytes[1] == 0xbb && bytes[2] == 0xbf) {
+            offset = 3;
+            length -= 3;
+        } else if (length > 2 && bytes[0] == 0xfe && bytes[1] == 0xff) {
+            offset = 2;
+            length -= 2;
+        } else if (length > 2 && bytes[0] == 0xff && bytes[1] == 0xfe) {
+            offset = 2;
+            length -= 2;
+        }
+
+        textFileSearch = new SuffixArrayTextFileSearch(new String(bytes, offset, length, charset));
     }
 
     @Override
